d4270c47d88ebdcd7a42e63d3c1c4b5f90abf1a1
Jannes Lambrecht <janneslambrecht@hotmail.com>
2022-11-03 17:25:44 +0100

    test

---
+++ week6/webdev/index.js
@@ -0,0 +1,18 @@
+// opvragen van moeilijkheid > opslaan in local storage
+
+// opvragen van aantal vragen > opslaan in local storage
+
+// opvragen van type vragen > opslaan in local storage
+
+// samenstellen API url
+
+// vragen ophalen (async) 
+
+// vraag 1 tonen > innerText 
+// antwoord opvragen > opslaan localstorage
+
+// na alle vragen
+// antwoorden controleren
+// score tonen
+
+
